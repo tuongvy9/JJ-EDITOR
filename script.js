674a24e75fbfe557785d45d8666306aeaaff1976
Vy you <98216975+tuongvy9@users.noreply.github.com>
2026-07-24 20:06:36 +0700

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 import * as THREE from 
 "https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js";
 
-import { OrbitControls } from 
-"https://cdn.jsdelivr.net/npm/three@0.160/examples/jsm/controls/OrbitControls.js";
-
-import { GLTFLoader } from 
-"https://cdn.jsdelivr.net/npm/three@0.160/examples/jsm/loaders/GLTFLoader.js";
-
-import { VRMLoaderPlugin } from
-"https://cdn.jsdelivr.net/npm/@pixiv/three-vrm@3.3.0/lib/three-vrm.module.js";100
-);
-
 
 camera.position.set(
 0,
