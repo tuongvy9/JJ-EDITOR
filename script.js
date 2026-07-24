9ee48adb831eec407a5666d250ec08cbb9771699
Vy you <98216975+tuongvy9@users.noreply.github.com>
2026-07-24 20:35:46 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160/build/three.module.js";
-
-import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.160/examples/jsm/loaders/GLTFLoader.js";
-
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.160/examples/jsm/controls/OrbitControls.js";
-
-import { VRMLoaderPlugin } from "https://cdn.jsdelivr.net/npm/@pixiv/three-vrm@2.1.0/lib/three-vrm.module.js";
