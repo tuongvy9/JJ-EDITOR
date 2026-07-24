4a1980788e20ad519c0058d15e3f2cca349681a4
Vy you <98216975+tuongvy9@users.noreply.github.com>
2026-07-24 21:53:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 import * as THREE from "./libs/three/three.module.js";
 
+import { GLTFLoader } from "./libs/three/GLTFLoader.js";
+
 import {
     VRMLoaderPlugin
 } from "./libs/three-vrm/three-vrm.module.js";
 
-console.log("Three.js loaded:", THREE);
-console.log("VRM plugin loaded:", VRMLoaderPlugin);
 
-
-// Tạo scene
+// Scene
 const scene = new THREE.Scene();
-
-scene.background = new THREE.Color(0x222222);
+scene.background = new THREE.Color(0x202020);
 
 
 // Camera
@@ -22,7 +20,7 @@ const camera = new THREE.PerspectiveCamera(
     100
 );
 
-camera.position.set(0, 1, 3);
+camera.position.set(0, 1.3, 3);
 
 
 // Renderer
@@ -55,37 +53,165 @@ light.position.set(
 scene.add(light);
 
 
+// VRM Loader
+const loader = new GLTFLoader();
+
+loader.register(
+    (parser) => {
+        return new VRMLoaderPlugin(parser);
+    }
+);
+
+
+let currentVRM = null;
+
+
+// Upload VRM
+const input = document.getElementById("vrmFile");
+
+
+if(input){
+
+input.addEventListener(
+"change",
+(event)=>{
+
+const file = event.target.files[0];
+
+if(!file) return;
+
+
+const url =
+URL.createObjectURL(file);
+
+
+loadVRM(url);
+
+});
+
+}
+
+
+
+function loadVRM(url){
+
+loader.load(
+
+url,
+
+(gltf)=>{
+
+
+const vrm =
+gltf.userData.vrm;
+
+
+if(!vrm){
+
+console.error(
+"Không phải file VRM"
+);
+
+return;
+
+}
+
+
+if(currentVRM){
+
+scene.remove(
+currentVRM.scene
+);
+
+}
+
+
+currentVRM = vrm;
+
+
+scene.add(
+vrm.scene
+);
+
+
+console.log(
+"VRM Loaded",
+vrm
+);
+
+
+},
+
+
+undefined,
+
+
+(error)=>{
+
+console.error(
+"VRM Load Error",
+error
+);
+
+}
+
+);
+
+}
+
+
+
 // Animation
+const clock =
+new THREE.Clock();
+
+
 function animate(){
 
-    requestAnimationFrame(
-        animate
-    );
+requestAnimationFrame(
+animate
+);
 
-    renderer.render(
-        scene,
-        camera
-    );
+
+if(currentVRM){
+
+currentVRM.update(
+clock.getDelta()
+);
+
 }
+
+
+renderer.render(
+scene,
+camera
+);
+
+}
+
 
 animate();
 
 
+
 // Resize
 window.addEventListener(
-    "resize",
-    ()=>{
+"resize",
+()=>{
 
-        camera.aspect =
-        window.innerWidth /
-        window.innerHeight;
 
-        camera.updateProjectionMatrix();
+camera.aspect =
+window.innerWidth /
+window.innerHeight;
 
-        renderer.setSize(
-            window.innerWidth,
-            window.innerHeight
-        );
 
-    }
+camera.updateProjectionMatrix();
+
+
+renderer.setSize(
+window.innerWidth,
+window.innerHeight
 );
+
+
+});
